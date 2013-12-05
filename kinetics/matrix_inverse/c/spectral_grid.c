0773748b125331c66ab7203941f743fb252e0b56
Evan Senter <evansenter@gmail.com>
2013-12-05 15:38:11 -0500

    Transition matrix in RNAspectral.

--- kinetics/matrix_inverse/c/spectral_grid.c
+++ kinetics/matrix_inverse/c/spectral_grid.c
@@ -7,17 +7,19 @@
   
 int main(int argc, char* argv[]) {
   int i, seq_length, num_structures = 0;
+  double** transition_matrix;
+  
   char* sequence  = argv[1];
   seq_length      = strlen(sequence);
-  char* empty_str = (char*)malloc((seq_length + 1) * sizeof(char));
+  char* empty_str = malloc((seq_length + 1) * sizeof(char));
   
   for (i = 0; i < seq_length; ++i) {
     empty_str[i] = '.';
   }
   empty_str[seq_length] = '\0';
   
-  SOLUTION* subopt_structures = subopt(sequence, empty_str, 1000000, NULL);
-  while (subopt_structures[num_structures].structure != NULL) {
+  SOLUTION* all_structures = subopt(sequence, empty_str, 1000000, NULL);
+  while (all_structures[num_structures].structure != NULL) {
     num_structures++;
   }
   
@@ -26,20 +28,39 @@ int main(int argc, char* argv[]) {
   printf("%d\n", num_structures);
   
   for (i = 0; i < num_structures; ++i) {
-    printf("%s\t%+.2f\n", subopt_structures[i].structure, subopt_structures[i].energy);
+    printf("%s\t%+.2f\n", all_structures[i].structure, all_structures[i].energy);
   }
+  
+  transition_matrix = convert_structures_to_transition_matrix(all_structures, num_structures);
   
   return 0;
 }
   
-char** get_all_structures_for_sequence(char* sequence) {
+double** convert_structures_to_transition_matrix(SOLUTION* all_structures, int num_structures) {  
+  int i, j;
+  double row_sum;
+  double** transition_matrix = malloc(num_structures * sizeof(double*));
   
-}
-
-double* get_all_energies_for_structures(char* sequence, char** structures) {
+  for (i = 0; i < num_structures; ++i) {
+    transition_matrix[i] = malloc(num_structures * sizeof(double));
+    row_sum              = 0;
+    
+    for (j = 0; j < num_structures; ++j) {
+      if (i != j) {
+        transition_matrix[i][j] = MIN(1., exp(-((double)all_structures[j].energy - (double)all_structures[i].energy) / RT));
+        row_sum                += transition_matrix[i][j];
+      }
+    }
+    
+    transition_matrix[i][i] = -row_sum;
+  }
   
-}
+  for (i = 0; i < num_structures; ++i) {
+    for (j = 0; j < num_structures; ++j) {
+      printf("%+.2f\t", transition_matrix[i][j]);
+    }
+    printf("\n");
+  }
   
-double** convert_sequence_to_transition_matrix(char* sequence) {
-  
+  return transition_matrix;
 }
--- kinetics/matrix_inverse/c/spectral_grid.h
+++ kinetics/matrix_inverse/c/spectral_grid.h
@@ -1,10 +1,8 @@
-char** get_all_structures_for_sequence(char* sequence);
-double* get_all_energies_for_structures(char* sequence, char** structures);
-double** convert_sequence_to_transition_matrix(char* sequence);
-
 typedef struct {
   float energy;       
   char *structure;    
 } SOLUTION;
 
 extern SOLUTION* subopt(char*, char*, int, FILE*);
+
+double** convert_structures_to_transition_matrix(SOLUTION*, int);
